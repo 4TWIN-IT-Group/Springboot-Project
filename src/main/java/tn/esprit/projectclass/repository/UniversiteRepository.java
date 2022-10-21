5a6b8c3909ba2034b29d8a5b5da5ba9d3440af36
Fritchou2 <mohamedaziz.ferchichi@esprit.tn>
2022-10-21 11:21:04 +0100

    clean

--- src/main/java/tn/esprit/projectclass/repository/UniversiteRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.projectclass.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.projectclass.entity.Universite;
-
-import java.io.Serializable;
-
-public interface UniversiteRepository extends JpaRepository<Universite, Serializable> {
-}
